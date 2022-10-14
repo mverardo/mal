88d338eb5e4cf21e26c19b3b4736a0b0a019f4cb
mverardo <mauricio.verardo@gmail.com>
2022-10-13 22:49:59 -0300

    Step 0

--- impls/risp/src/bin/step0_repl.rs
+++ impls/risp/src/bin/step0_repl.rs
@@ -1,3 +1,34 @@
+use std::io::Write;
+
+fn read(s: String) -> String {
+    s
+}
+
+fn eval(s: String) -> String {
+    s
+}
+
+fn print(s: String) -> String {
+    s
+}
+
+fn rep(s: String) -> String {
+    print(eval(read(s)))
+}
+
 fn main() {
-    println!("Hello, new world!");
+    loop {
+        let mut input = String::new();
+        print!("user> ");
+        std::io::stdout().flush().unwrap();
+
+        let bytes_read = std::io::stdin().read_line(&mut input);
+
+        if matches!(bytes_read, Ok(0)) {
+            break;
+        };
+
+        let rep_result = rep(input);
+        print!("{}", rep_result);
+    }
 }
